86d80a8770d2c4e161bb11283344de9c8ca54935
muhammadaffan <muhammadaffanpvt@gmail.com>
2026-04-17 21:59:03 +0530

    repo init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import FetchApi from "./useeffectPractice/FetchApi"
+import Debouncing from "./useeffectPractice/Debouncing"
+function App() {
+  return (
+    <>
+      <Debouncing />
+    </>
+  )
+}
+
+export default App
